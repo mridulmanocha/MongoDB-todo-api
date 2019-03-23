38602ab3ce5212d9f309d71eba221557f627f864
mridulmanocha <mridul.manocha22@gmail.com>
2019-03-23 16:14:28 +0530

    test cases for GET request

--- server/testing/server.test.js
+++ server/testing/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 const {todo} = require('./../models/todo.js');
 
+const todos = [{
+	text : 'first todo'
+},
+{
+	text : 'second todo'
+}];
+
 beforeEach((done) => {
-	todo.remove({}).then(() => done());
+	todo.remove({}).then(() => {
+		return todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST todo' , () => {
@@ -26,11 +35,44 @@ describe('POST todo' , () => {
 			}
 
 			todo.find().then((result) => {
-				expect(result.length).toBe(1);
-				expect(result[0].text).toBe(text);
+				expect(result.length).toBe(3);
+				expect(result[2].text).toBe(text);
 				done();
 			}).catch((error) => done(error));
  		});
 
 	});
+
+	it('should not create a new todo with invalid data' , (done) => {
+
+		request(app)
+		.post('/todos')
+		.send({})
+		.expect(400)
+		.end((error , res) => {
+			if (error) {
+				return done(error);
+			} else {
+
+				todo.find().then((result) => {
+					expect(result.length).toBe(2);
+					done();
+				}).catch((error) => done(error));
+			};
+		});
+	});
+});
+
+describe('GET todo' , () => {
+	it('should display todos list on /todo' , (done) => {
+
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.result.length).toBe(2);
+		})
+		.end(done);
+	});
+		
 });
