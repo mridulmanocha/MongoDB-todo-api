43ff59a9c9f0110486b4ec335b0380011aa479db
mridulmanocha <mridul.manocha22@gmail.com>
2019-03-30 15:13:26 +0530

    find by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {todo} = require('./models/todo.js');
@@ -27,6 +28,26 @@ app.get('/todos' , (req , res) => {
 		res.send({result})
 	}, (error) => {
 		res.status(400).send(error);
+	});
+});
+
+app.get('/todos/:id' , (req , res) => {
+	var id = req.params.id;
+	
+	if (!ObjectID.isValid(id)){
+		return res.status(404).send();
+	} 
+
+	todo.findById(id).then((todo) => {
+
+			if(!todo) {
+				return res.status(404).send();
+			}
+
+			res.send(todo);
+
+	}).catch((e) => {
+		res.status(404).send();
 	});
 });
 
